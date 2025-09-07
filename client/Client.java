46f0a5dfdca025c36f81980e79dfa516cd4a93f0
Pavelv15 <pashokwolf15@mail.ru>
2025-09-07 16:14:30 +0300

    +Исправление построчной отправки данных

--- client/Client.java
+++ client/Client.java
@@ -3,55 +3,28 @@ package client;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.Charset;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Client {
-    static  void otpravka(String s,Socket cs ) throws IOException {
+    static  void send(String s,Socket cs ) throws IOException {
        OutputStreamWriter writer = new OutputStreamWriter(cs.getOutputStream(), Charset.forName("UTF-8"));
         writer.write(s);
        writer.flush();
     }
     public static void main(String[] args) throws IOException {
         try(Socket cs = new Socket("localhost",1111)) {
-
             String out = "";
-
-
-            try(FileReader reader = new FileReader("test2.txt"))
-            {
-                // читаем посимвольно
-
+            try(FileReader reader = new FileReader("out.txt"))
+            {// читаем посимвольно
                int c;
-
                 while((c=reader.read())!=-1){
-
                    out = out + (char) c;
-
                 }
-
-
-
-
             }
-            //out = out.replace("\n"," ");
             System.out.println(out);
-            String[] massive = out.split("\n");
-
-
-            for (int i =0 ; i < massive.length; i++) {
-                otpravka(massive[i],cs);
-
-
-            }
-
-
-
-
-
+            send(out.replaceAll("\r\n","PRN") +"\n",cs);
             BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream(),Charset.forName("UTF-8")));
             System.out.println(reader.readLine());
-
         }
     }
 }
--- server/Server.java
+++ server/Server.java
@@ -9,49 +9,42 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Server {
-
     public static void main(String[] args) throws IOException, InterruptedException {
         ServerSocket ss = new ServerSocket(1111);
         ExecutorService p = Executors.newCachedThreadPool();
         AtomicInteger req = new AtomicInteger();
-
         while (true) {
             final  Socket cs = ss.accept();
             Runnable task = () -> {
                 try {
-
-
+                    int id = req.incrementAndGet();
+                    System.out.printf("Сессия id = %d\n",id);
                     System.out.printf("Соединение установлено c адреса %s\n", cs.getInetAddress().toString());
                     BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream(), Charset.forName("UTF-8")));
-                    String s = reader.readLine();
-                    System.out.printf("%s . %d\n", s,req.incrementAndGet());
-
-                    try(FileWriter writer = new FileWriter("test.txt", true))
+                    String s = reader.readLine().replaceAll("PRN","\r\n");
+                    System.out.println("Получены следующие текстовые данные, которй будут сохранены в файл in.txt");
+                    System.out.printf("%s", s);
+                    System.out.println("\nСессия id=" + id + " завершена");
+                    try(FileWriter writer = new FileWriter("in.txt", true))
                     {
                         // запись всей строки
-
+                        writer.write("\n");
                         writer.write(s);
                         //writer.append("\n");
                         OutputStreamWriter out = new OutputStreamWriter(cs.getOutputStream(), Charset.forName("UTF-8"));
                         out.write("Ответ принят\n");
                         out.flush();
-
                         writer.flush();
                     }
                     catch(IOException ex){
-
                         System.out.println(ex.getMessage());
                     }
                 }
                 catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
-
             };
-
             p.submit(task);
-
-
         }
     }
 }
